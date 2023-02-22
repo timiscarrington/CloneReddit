d572844224317d0995974610af9f9a21e593135e
TimisCarrington <timiscarrington@gmail.com>
2023-02-22 00:06:23 -0800

    made changes to searchinput.tsx due to build error during deployment

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -4,35 +4,36 @@ import { User } from 'firebase/auth';
 import React from 'react';
 
 type SearchInputProps = {
-user ?: User | null;
+  user?: User | null;
 };
 
-const SearchInput:React.FC<SearchInputProps> = ({ user }) => {
-    return (
-        <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"}  mr={2} align="center" >
-            <InputGroup>
-                <InputLeftElement
-                    pointerEvents='none'
-                    children={<SearchIcon color='gray.400' mb={1}/>} />
-                <Input 
-                placeholder="Search Reddit" 
-                fontSize='10pt'
-                 _placeholder={{ color: "gray.500"}}
-                _hover={{
-                    br: "white",
-                    border: "1px solid",
-                    borderColor: "blue.500"
-                }}
-                _focus={{
-                    outline: "none",
-                    border: "1px solid",
-                    borderColor: "blue.500",
-                }}
-                height="34px"
-                bg="gray.50"
-                />
-            </InputGroup>
-        </Flex>
-    );
-}
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
+  return (
+    <Flex flexGrow={1} maxWidth={user ? 'auto' : '600px'} mr={2} align="center">
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon color="gray.400" mb={1} />
+        </InputLeftElement>
+        <Input
+          placeholder="Search Reddit"
+          fontSize="10pt"
+          _placeholder={{ color: 'gray.500' }}
+          _hover={{
+            br: 'white',
+            border: '1px solid',
+            borderColor: 'blue.500',
+          }}
+          _focus={{
+            outline: 'none',
+            border: '1px solid',
+            borderColor: 'blue.500',
+          }}
+          height="34px"
+          bg="gray.50"
+        />
+      </InputGroup>
+    </Flex>
+  );
+};
+
 export default SearchInput;
